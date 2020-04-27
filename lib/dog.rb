58a3a09c49e5d1099f5b3c60f1100acf09e2be30
Joey <joey.alzamora@gmail.com>
2020-04-27 21:05:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog 
 
   def name=(dog_name)
-    @this_dogs_name =    #### setter method 
+    @name =    #### setter method 
     dog_name
   end
   
   def name
-    @this_dogs_name     #### getter method 
+    @name     #### getter method 
   end
 
   def breed=(dogs_breed)
